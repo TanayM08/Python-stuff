a493216776f02f094d29c3f4349464df3e962ab3
Tanay Malde <78842021+TanayM08@users.noreply.github.com>
2021-10-07 21:14:17 +0300

    simple interest
    simple interest calculator

---
+++ simple interest.py
@@ -0,0 +1,10 @@
+#qn3 program to calculate simple interest
+def si(p ,n ,r):
+    si=(p*n*r)/100
+
+p=float(input('Enter the principal amount\n'))
+n=float(input('Enter the time amount\n'))
+r=float(input('Enter the rate of interest\n'))
+
+si=si(p, n, r)
+print('your simple interest is',si)
